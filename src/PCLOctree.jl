b950c1efd747ef486f49dd8fe8e03984db53e804
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-04-17 22:05:01 +0900

    Octree

--- src/PCLOctree.jl
+++ src/PCLOctree.jl
@@ -1,5 +1,16 @@
 module PCLOctree
 
-# package code goes here
+using LibPCL
+using PCLCommon
+using Cxx
+
+const libpcl_octree = LibPCL.find_library_e("libpcl_octree")
+try
+    Libdl.dlopen(libpcl_octree, Libdl.RTLD_GLOBAL)
+catch e
+    warn("You might need to set DYLD_LIBRARY_PATH to load dependencies proeprty.")
+    rethrow(e)
+end
+
 
 end # module
